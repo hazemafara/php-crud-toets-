7de5b5fe8b4b7e10cf93a4b3191538322665cf87
hazemafara <566508@edu.rocmn.nl>
2023-01-13 11:53:09 +0100

    wrf

---
+++ read.php
@@ -0,0 +1,18 @@
+<?php
+
+
+$dsn = "mysql:host=$dbHost;dbname=$dbName;charset=UTF8";
+
+try {
+    $pdo = new PDO($dsn, $dbUser, $dbPass);
+    if ($pdo) {
+        // echo "De verbinding is gelukt";
+    } else {
+        echo "Interne server-error";
+    }
+} catch (PDOException $e) {
+    echo $e->getMessage();
+}
+
+
+
